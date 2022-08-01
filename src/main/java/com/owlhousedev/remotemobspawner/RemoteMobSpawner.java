b64c5d0de69260d272fdf2be009a427f307dd561
gitakmin <takujomu@gmail.com>
2022-08-02 00:39:49 +0900

    Initial commit

---
+++ src/main/java/com/owlhousedev/remotemobspawner/RemoteMobSpawner.java
@@ -0,0 +1,17 @@
+package com.owlhousedev.remotemobspawner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RemoteMobSpawner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
